bf9d9df64560c14e0cced5d140ac95d643bde0ca
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-04-05 18:45:40 -0400

    Update WavPackHybridStereoDecider.jl

--- src/WavPackHybridStereoDecider.jl
+++ src/WavPackHybridStereoDecider.jl
@@ -1,6 +1,18 @@
 ###############################
 # StereoDecision.jl
 ###############################
+# Assumes:
+# - WavpackStream has fields:
+#     w::WordsData
+#     dc.shaping_acc::NTuple{2,Int32}
+#     dc.shaping_delta::NTuple{2,Int32}
+#     dc.shaping_array::Union{Nothing,AbstractVector}
+#     wphdr.flags::UInt32
+
+############################################################
+# 1. Parameter structs (no magic constants)
+############################################################
+
 struct MedianParams
     div0::UInt32
     div1::UInt32
@@ -27,11 +39,18 @@ mutable struct EntropyData
 end
 
 mutable struct WordsData
-    c::NTuple{2,EntropyData}   # one per channel
+    bitrate_delta::NTuple{2,UInt32}
+    bitrate_acc::NTuple{2,UInt32}
+    pend_data::UInt32
+    holding_one::UInt32
+    zeros_acc::UInt32
+    holding_zero::Int32
+    pend_count::Int32
+    c::NTuple{2,EntropyData}
 end
 
 ############################################################
-# 3. Median helpers (Julian, pure, readable)
+# 3. Median helpers
 ############################################################
 
 @inline get_med(m::UInt32) = (m >> 4) + 1
@@ -47,22 +66,24 @@ end
 ############################################################
 
 function update_error_limit!(wps)
-    # TODO: port from C
+    # TODO: port from C:
+    #   void update_error_limit (WavpackStream *wps)
     return
 end
 
 ############################################################
-# 5. nosend_word (clean, Julian, bit-exact)
+# 5. nosend_word (bit-exact, Julian style)
 ############################################################
 
 function nosend_word(wps, value::Int32, chan::Int)
-    c = wps.w.c[chan + 1]
+    wd = wps.w::WordsData
+    c  = wd.c[chan + 1]
 
-    # --- sign folding ---
+    # sign folding
     sign = value < 0
     v = sign ? ~value : value
 
-    # hybrid-lossless always updates error limit on channel 0
+    # hybrid-lossless only: always update error limit on chan 0
     if chan == 0
         update_error_limit!(wps)
     end
@@ -94,9 +115,9 @@ function nosend_word(wps, value::Int32, chan::Int)
                 m2   = dec_med(m2, MEDIANS.div2)
             else
                 ones = UInt32(2 + dv2 ÷ Int32(get_med(m2)))
-                low += (ones - 2) * get_med(m2)
-                high = low + get_med(m2) - 1
-                m2   = inc_med(m2, MEDIANS.div2)
+                low  += (ones - 2) * get_med(m2)
+                high  = low + get_med(m2) - 1
+                m2    = inc_med(m2, MEDIANS.div2)
             end
         end
     end
@@ -127,104 +148,249 @@ function nosend_word(wps, value::Int32, chan::Int)
 end
 
 ############################################################
-# 6. Noisy buffer (clean, Julian)
+# 6. Decorrelation spec
 ############################################################
 
-function stereo_add_noise!(wps, noisy, orig)
-    acc0, acc1 = wps.dc.shaping_acc
-    δ0,   δ1   = wps.dc.shaping_delta
-    arr        = wps.dc.shaping_array
-    is_new     = (wps.wphdr.flags & NEW_SHAPING) != 0
+struct DecorrSpec{N}
+    joint_stereo::Bool
+    delta::Int32
+    terms::NTuple{N,Int}
+end
 
-    e0 = Int32(0)
-    e1 = Int32(0)
-
-    @inbounds for i in eachindex(orig)
-        s = orig[i]
-
-        # --- channel 0 ---
-        w0 = arr === nothing ? (acc0 += δ0; acc0 >> 16) : Int32(arr[i])
-        t0 = -apply_weight(w0, e0)
-        q0 = nosend_word(wps, s.l, 0)
-        base0 = q0 - s.l
-
-        if is_new && w0 < 0 && t0 != 0 && t0 == e0
-            t0 += t0 < 0 ? 1 : -1
-        end
-
-        e0 = base0 + t0
-        L  = s.l + e0
-
-        # --- channel 1 ---
-        w1 = (acc1 += δ1) >> 16
-        t1 = -apply_weight(w1, e1)
-        q1 = nosend_word(wps, s.r, 1)
-        base1 = q1 - s.r
-
-        if is_new && w1 < 0 && t1 != 0 && t1 == e1
-            t1 += t1 < 0 ? 1 : -1
-        end
-
-        e1 = base1 + t1
-        R  = s.r + e1
-
-        noisy[i] = Stereo{Int32}(L, R)
+function load_specs_from_tables(tables::Dict{Symbol,Vector}, key::Symbol)
+    raw = tables[key]
+    specs = DecorrSpec[]
+    for (joint, delta, terms) in raw
+        N = length(terms)
+        tnt = ntuple(i -> Int(terms[i]), N)
+        push!(specs, DecorrSpec{N}(joint != 0, Int32(delta), tnt))
     end
-
-    wps.dc.shaping_acc = (acc0, acc1)
-    return noisy
+    return specs
 end
 
 ############################################################
-# 7. Decorrelator trial
+# 7. wp_log2 (bit-exact)
 ############################################################
 
-function trial_chain(data, memories)
-    states = make_states(memories)
-    bufs   = make_buffers(memories)
+@inline function wp_log2(avalue::UInt32)
+    av = avalue + (avalue >> 9)
 
+    if av < 0x100
+        dbits = nbits_table[Int(av) + 1]
+        idx   = Int((av << (9 - dbits)) & 0xff) + 1
+        return (UInt32(dbits) << 8) + UInt32(log2_table[idx])
+    else
+        dbits::Int
+        if av < 0x10000
+            dbits = nbits_table[Int(av >> 8) + 1] + 8
+        elseif av < 0x1000000
+            dbits = nbits_table[Int(av >> 16) + 1] + 16
+        else
+            dbits = nbits_table[Int(av >> 24) + 1] + 24
+        end
+
+        idx = Int((av >> (dbits - 9)) & 0xff) + 1
+        return (UInt32(dbits) << 8) + UInt32(log2_table[idx])
+    end
+end
+
+############################################################
+# 8. log2buffer (bit-exact)
+############################################################
+
+@inline function log2buffer(res::Vector{Stereo{Int32}}; limit::UInt32 = 0)
+    acc = UInt32(0)
+
+    @inbounds for s in res
+        for v in (s.l, s.r)
+            av = UInt32(abs(v))
+            val = wp_log2(av)
+
+            if limit != 0 && val >= limit
+                return typemax(UInt32)
+            end
+
+            acc += val
+        end
+    end
+
+    return acc
+end
+
+############################################################
+# 9. Mid/side transform
+############################################################
+
+function to_joint_stereo(data::Vector{Stereo{Int32}})
     out = similar(data)
     @inbounds for i in eachindex(data)
-        s = data[i]
-        s, states = process_chain!(memories, states, bufs, s)
-        out[i] = s
+        L = data[i].l
+        R = data[i].r
+        mid  = Int32(L - R)
+        side = Int32(R + (mid >>> 1))
+        out[i] = Stereo{Int32}(mid, side)
     end
     return out
 end
 
 ############################################################
-# 8. Stereo mode chooser (clean, modern)
+# 10. Noisy buffer (bit-exact stereo_add_noise, hybrid only)
 ############################################################
 
-function choose_stereo_mode!(wps, orig, specs)
-    noisy = similar(orig)
-    stereo_add_noise!(wps, noisy, orig)
+function stereo_add_noise!(wps, noisy::Vector{Stereo{Int32}},
+                           orig::Vector{Stereo{Int32}})
+    flags = wps.wphdr.flags
+    is_new = (flags & NEW_SHAPING) != 0
 
-    best_cost = typemax(UInt64)
-    best_res  = nothing
+    shaping_array = wps.dc.shaping_array
+
+    error0 = Int32(0)
+    error1 = Int32(0)
+
+    cnt = length(orig)
+
+    # hybrid lossless only: always use shaping path
+    acc0 = wps.dc.shaping_acc[1]
+    acc1 = wps.dc.shaping_acc[2]
+    δ0   = wps.dc.shaping_delta[1]
+    δ1   = wps.dc.shaping_delta[2]
+
+    idx_sa = 1
+
+    @inbounds for i in 1:cnt
+        s = orig[i]
+
+        # ----- channel 0 -----
+        shaping_weight0 = shaping_array !== nothing ?
+            Int32(shaping_array[idx_sa]) :
+            begin acc0 += δ0; acc0 >> 16 end
+
+        temp0 = -apply_weight(shaping_weight0, error0)
+        q0    = nosend_word(wps, s.l, 0)
+        base_err0 = q0 - s.l
+
+        if is_new && shaping_weight0 < 0 && temp0 != 0
+            if temp0 == error0
+                temp0 += temp0 < 0 ? 1 : -1
+            end
+            error0 = base_err0 + temp0
+        else
+            error0 = base_err0 + temp0
+        end
+
+        noisyL = s.l + error0
+
+        # ----- channel 1 -----
+        shaping_weight1 = begin acc1 += δ1; acc1 >> 16 end
+
+        temp1 = -apply_weight(shaping_weight1, error1)
+        q1    = nosend_word(wps, s.r, 1)
+        base_err1 = q1 - s.r
+
+        if is_new && shaping_weight1 < 0 && temp1 != 0
+            if temp1 == error1
+                temp1 += temp1 < 0 ? 1 : -1
+            end
+            error1 = base_err1 + temp1
+        else
+            error1 = base_err1 + temp1
+        end
+
+        noisyR = s.r + error1
+
+        noisy[i] = Stereo{Int32}(noisyL, noisyR)
+
+        if shaping_array !== nothing
+            idx_sa += 1
+        end
+    end
+
+    if shaping_array === nothing
+        acc0 -= δ0 * cnt
+        acc1 -= δ1 * cnt
+    end
+
+    wps.dc.shaping_acc = (acc0, acc1)
+
+    return noisy
+end
+
+############################################################
+# 11. Single decorrelation trial
+############################################################
+
+function trial_chain(data::Vector{Stereo{Int32}}, memories; init_weight::Int32 = 0)
+    states = make_states(memories; init_weight)
+    bufs   = make_buffers(memories)
+
+    n = length(data)
+    res = Vector{Stereo{Int32}}(undef, n)
+
+    s = Stereo{Int32}(0, 0)
+    @inbounds for i in 1:n
+        s = data[i]
+        s, states = process_chain!(memories, states, bufs, s)
+        res[i] = s
+    end
+
+    return res
+end
+
+############################################################
+# 12. Stereo mode chooser (uses noisy buffer)
+############################################################
+
+function choose_stereo_mode!(
+    wps,
+    orig_samples::Vector{Stereo{Int32}},
+    specs::Vector{DecorrSpec};
+    num_passes::Int = 1,
+    init_weight::Int32 = 0,
+    force_js::Bool = false,
+    force_ts::Bool = false,
+)
+
+    n = length(orig_samples)
+
+    noisy = similar(orig_samples)
+    stereo_add_noise!(wps, noisy, orig_samples)
+
+    all_zero = all(s -> s.l == 0 && s.r == 0, noisy)
+    if all_zero
+        return noisy, nothing, false
+    end
+
+    best_size = typemax(UInt64)
+    best_res  = Vector{Stereo{Int32}}(undef, n)
     best_mem  = nothing
     best_js   = false
 
     js_buf = nothing
 
-    for spec in specs
-        use_js = spec.joint_stereo
+    for pass in 1:num_passes
+        for spec in specs
+            use_js = force_js || (spec.joint_stereo && !force_ts)
 
-        trial_in = use_js ?
-            (js_buf === nothing ? (js_buf = to_joint_stereo(noisy)) : js_buf) :
-            noisy
+            trial_in = if use_js
+                js_buf === nothing && (js_buf = to_joint_stereo(noisy))
+                js_buf
+            else
+                noisy
+            end
 
-        deltas = ntuple(_ -> spec.delta, length(spec.terms))
-        memories = make_memories(Int32, spec.terms, deltas)
+            N = length(spec.terms)
+            deltas = ntuple(_ -> spec.delta, N)
+            memories = make_memories(Int32, spec.terms, deltas)
+            res = trial_chain(trial_in, memories; init_weight)
 
-        res = trial_chain(trial_in, memories)
-        cost = UInt64(log2buffer(res))
+            size = UInt64(log2buffer(res))
 
-        if cost < best_cost
-            best_cost = cost
-            best_res  = res
-            best_mem  = memories
-            best_js   = use_js
+            if size < best_size
+                best_size = size
+                best_res  = res
+                best_mem  = memories
+                best_js   = use_js
+            end
         end
     end
 
